f73e41aeca5d48aa1e64fbc90cbc06f0be3d26d3
Alexandros Kogkos <akogos@192.168.2.9>
2026-07-27 17:20:09 +0300

    Refine formal clinic typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const notoSans = Noto_Sans({
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
-  weight: ["700"],
+  weight: ["600", "700"],
 });
 
 const openSans = Open_Sans({
